658049ee7b934a7e102876f06b1ed66be30e91d0
ssagara00 <barabeikaso@yahoo.co.jp>
2023-02-08 18:52:59 +0900

    test

--- react/react-app/jest.config.js
+++ react/react-app/jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  /*preset: 'ts-jest',*/
+  testEnvironment: 'jsdom',
+  roots: ["<rootDir>/src"],
+  "moduleNameMapper": {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  transform: {
+    "^.+\\.tsx?$": "@swc/jest"
+  }
 };
