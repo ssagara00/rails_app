98a0863ab5842e299d0cfcbbc769a62e270532a0
ssagara00 <barabeikaso@yahoo.co.jp>
2023-04-24 13:09:59 +0900

    for circleci

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = ENV['AWS_BUCKET_NAME']
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['BUCKET_AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['BUCKET_AWS_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['AWS_REGION'],
       path_style: true
     }
